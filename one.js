f112591433f5211bb16a64a7a672127f63a996b5
sky91949 <42425403+sky91949@users.noreply.github.com>
2018-08-20 15:54:52 +0800

    Update one.js

--- one.js
+++ one.js
@@ -9,7 +9,7 @@ const regtest = bitcoin.networks.testnet
 const bip65 = require('bip65')
 
 //輸入私鑰，教學用，請勿隨意公開私鑰
-var privateKey = "cNcvQphXWjAJ365Y8Tuhti5a6fCh9ftpR3GScZs5vdidBwQqpxT4";
+var privateKey = "cN4aXkbu3baz53RyZRHg41Ga87WrYoF6oiUExcjmtQTnaKELCLKG";
 
 //產生公鑰跟私鑰
 const keyPair = bitcoin.ECPair.fromWIF(privateKey,regtest);
@@ -21,7 +21,7 @@ const txb = new bitcoin.TransactionBuilder(regtest);
 
 
 //用listunspent 取出最後一筆資料的txid
-txb.addInput('c3acac43c6f0e83538fb5c82ed01379349f5dccf43a8af7bb5d8f0c60fb1fcfc', 0);
+txb.addInput('5d14fe85719e63aa4eb1df911b864298c4ea965ce2e8198054fc48009f588273', 0);
 
 //用getnewaddress取得新的位址
 txb.addOutput('2MxfNS8sB1GcCo3GBLmX2PUw277H9zvrn2L', 12)
